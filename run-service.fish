2b3132238b0332aa1bf6ae13faf8d2101f0cfb22
Rok Pajnic <rok.pajnic@outlook.com>
2023-11-12 18:34:50 +0100

    add jellyfin devices for hw transcoding

--- run-service.fish
+++ run-service.fish
@@ -2,7 +2,7 @@
 
 # Check if argument is provided
 if test -z "$argv[1]" -o -z "$argv[2]"
-    echo "Usage: docker-service.sh <start|stop|restart> <service>"
+    echo "Usage: docker-service.sh <start|stop|restart|down> <service>"
     exit 1
 end
 
@@ -11,8 +11,8 @@ set command $argv[1]
 set service $argv[2]
 
 # Check if command is valid
-if not contains $command start stop restart
-  echo "Invalid command. Use start, stop or restart."
+if not contains $command start stop restart down
+  echo "Invalid command. Use start, stop, down or restart."
   exit 1
 end
 
@@ -30,6 +30,8 @@ switch $command
     docker compose -f $service/docker-compose.yml stop
   case "restart"
     docker compose -f $service/docker-compose.yml restart
+  case "down"
+    docker compose -f $service/docker-compose.yml down
 end
 
 echo "Docker service '$service' has been $command-ed."
